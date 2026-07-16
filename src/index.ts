50f444d49dfefa8217f6818fa5c5b7602e8bd081
sbokhari <bokharisaab12345@gmail.com>
2026-07-16 16:09:02 -0400

    directory read

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("hello world")
+import fs from 'node:fs/promises';
+import path from 'node:path';
+
+async function readDirectory(dirPath: string) {
+  try {
+    // Reads all file and folder names inside the directory
+    const files = await fs.readdir(dirPath);
+    
+    files.forEach(file => {
+      console.log(file);
+    });
+  } catch (error) {
+    console.error('Error reading directory:', error);
+  }
+}
+
+readDirectory('./');
